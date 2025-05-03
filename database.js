90ddb116995cdbf986ede6e7fbff79429787bd70
WibiProg <143897423+wibice@users.noreply.github.com>
2025-05-03 00:17:02 -0300

    New project node

---
+++ database.js
@@ -0,0 +1,27 @@
+import { create } from "domain"
+import { randomUUID } from "crypto"
+
+//UUID = Universal Unique ID
+
+export class DatabeseMemory {
+    #videos = new Map()
+  
+    list(){
+      return this.#videos.values(id)
+    }
+    
+  create (videos) {
+    const videoId = randomUUID()
+    this.#videos.set(videoId, videos)
+
+  }
+
+  updade (id, videos) { //
+    this.#videos.set(videos)
+  }
+
+  delete (id) {
+    this.videos.delete(id)
+  }
+
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+import fastify from "fastify";
+import { DatabeseMemory } from "./database.js";
+
+const server = fastify()
+
+
+//*Record create
+server.post('/videos', () => {
+ 
+  return {videos_create: "create videos"}
+})
+ 
+//?picking up details
+server.get('/videos', () => {
+  return {detls: "Hellow Details" }
+})
+
+//-change 
+server.put('/videos:id', () =>{
+  return {change: 'videos'}
+})
+
+
+
+server.listen({
+  port: 3333,
+
+})
